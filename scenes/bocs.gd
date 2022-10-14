28440e0f618f4ec5864e86b657a53fcdeb6e9e80
Elías Zelada <elixs.777@gmail.com>
2022-10-14 12:48:18 -0300

    yield example

--- scenes/bocs.gd
+++ scenes/bocs.gd
@@ -13,6 +13,8 @@ func _ready():
 	tween.tween_property(self, "frequency", 10, 5).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUINT)
 	tween.tween_property(self, "frequency", 2, 2).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 
+	#yield(tween, "finished")
+	
 func _physics_process(delta):
 	print(amplitude)
 	delta_acc += delta * frequency
